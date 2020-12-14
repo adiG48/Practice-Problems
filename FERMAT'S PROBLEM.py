10a722a793979122181ddc0c49a3554e68921b81
adiG48 <72435206+adiG48@users.noreply.github.com>
2020-12-14 10:50:03 +0530

    Update FERMAT'S PROBLEM.py

--- FERMAT'S PROBLEM.py
+++ FERMAT'S PROBLEM.py
@@ -1,3 +1,13 @@
+"""Fermat’s Last Theorem says that there are no positive integers a, b, and c such that
+an+ bn= cn
+ for any values of n greater than 2
+1. Write a function named check_fermat that takes four parameters a, b, c and n
+- and checks to see if Fermat’s theorem holds. If n is greater than 2 and an +bn = cn
+ the program should print, “Holy smokes, Fermat was wrong!”
+Otherwise the program should print, “No, that doesn’t work.”
+2. Write a function that prompts the user to input values for a, b, c and n,
+converts them to integers, and uses check_fermat to check whether they
+violate Fermat’s theorem"""
 def get_input():
     a = int(input('What value to use for a?\n'))
     b = int(input('What value to use for b?\n'))
